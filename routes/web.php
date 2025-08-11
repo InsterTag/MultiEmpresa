97c0ba5524ec513eeb67bfdf64d1ec96355f6690
gomez-alejo <acometagomez@gmail.com>
2025-08-11 10:03:00 -0500

    se implemento la vista de productos con sus respectivos componentes

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@ Route::get('/', function () {
 
 //products
 Route::get('/productos', function () {
-    return view('welcome');
+    return view('products.products');
 })->name('productos');
+
 
 
 
@@ -35,9 +36,6 @@ Route::middleware('auth')->group(function () {
 
 
     Route::get('/user', function () {
-
-//usuarios
-Route::get('/user', function () {
     return view('profiles.profile');
 })->name('user');
 
@@ -65,24 +63,6 @@ Route::get('/company', function () {
 // Route::get('/register/company', [CompanyAuthController::class, 'showRegisterForm'])->name('register.company');
 // Route::post('/login/company', [CompanyAuthController::class, 'login']);
 // Route::post('/register/company', [CompanyAuthController::class, 'register']);
-
-//perfil usuario
-Route::get('/userprofile', function () {
-    return view('profiles.user.profile');
-})->name('userprofile');
-
-
-// === Usuario ===
-Route::get('/login/user', [UserAuthController::class, 'showLoginForm'])->name('login.user');
-Route::get('/register/user', [UserAuthController::class, 'showRegisterForm'])->name('register.user');
-Route::post('/login/user', [UserAuthController::class, 'login']);
-Route::post('/register/user', [UserAuthController::class, 'register']);
-
-// === Empresa ===
-Route::get('/login/company', [CompanyAuthController::class, 'showLoginForm'])->name('login.company');
-Route::get('/register/company', [CompanyAuthController::class, 'showRegisterForm'])->name('register.company');
-Route::post('/login/company', [CompanyAuthController::class, 'login']);
-Route::post('/register/company', [CompanyAuthController::class, 'register']);
 
 // Logout (puedes reutilizar uno solo)
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
