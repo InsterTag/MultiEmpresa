10a9606a23e45aa804d5489a4f8f7c7dbeac04dd
gomez-alejo <acometagomez@gmail.com>
2025-08-12 11:21:15 -0500

    xdlinks

--- resources/views/profiles/admin/admin.blade.php
+++ resources/views/profiles/admin/admin.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Super Admin Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lucide/0.263.1/umd/lucide.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/lucide@latest/dist/umd/lucide.js"></script>
+
     @vite('resources/css/admin.css')
 </head>
 <body class="custom-bg-light min-h-screen">
